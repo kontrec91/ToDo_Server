5e0b81d24e424f0636000ba7221673c941097c4b
kontrec91 <kontrec91@gmail.com>
2021-12-28 15:43:29 +0200

    server vs db

--- app.js
+++ app.js
@@ -1,7 +1,67 @@
+// import { MongoClient } from "./db.js";
+
+// const http = require("http");
+
+// const hostname = "127.0.0.1";
+// const port = 3000;
+
+// const arr = [];
+
+// const server = http.createServer((request, response) => {
+//   response.statusCode = 200;
+//   response.setHeader("Content-Type", "text/plain");
+//   response.setHeader("Content-Type", "application/json");
+//   response.setHeader("Access-Control-Allow-Origin", "*");
+//   response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   response.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "X-Requested-With,content-type"
+//   );
+
+//   response.setHeader("Access-Control-Allow-Credentials", true);
+
+//   let body = "";
+//   if (request.method === "POST") {
+//     request.on("data", (chunk) => {
+//       console.log('A chunk of data has arrived: ', chunk);
+//       // body += JSON.stringify(chunk);
+//       body += chunk;
+//       arr.push(body)
+//       response.end(JSON.stringify(arr));
+//     });
+//   } else if (request.method === "GET") {
+//     response.end(JSON.stringify(arr));
+//   }
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+// const MongoClient = require("mongodb").MongoClient;
+// // const url = "mongodb://localhost/Blog";
+// const url = "mongodb://localhost:27017/";
+
+// console.log("yes")
+
+// http.createServer(function (req, res) {
+//   console.log(1)
+//   MongoClient.connect(url, function(err, db) {
+//       if (err) throw err;
+//       console.log(db)
+//       console.log(2)
+//   });
+// }).listen(27017);
+
 const http = require("http");
+const mongoClient = require("mongodb").MongoClient;
+const bodyParser = require('body-parser')
 
 const hostname = "127.0.0.1";
 const port = 3000;
+
+const dbName = "ToDoDatabase";
+const uri = `mongodb://127.0.0.1:27017/${dbName}`;
 
 const arr = [];
 
@@ -19,21 +79,54 @@ const server = http.createServer((request, response) => {
   response.setHeader("Access-Control-Allow-Credentials", true);
 
   let body = "";
-  if (request.method === "POST") {
-    request.on("data", (chunk) => {
-      console.log('A chunk of data has arrived: ', chunk);
-      // body += JSON.stringify(chunk);
-      body += chunk;
-      arr.push(body)
-      response.end(JSON.stringify(arr));
-    });
-  } else if (request.method === "GET") {
-    response.end(JSON.stringify(arr));
-  }
+  let colletction = "ToDoCollectionTodos";
+
+  mongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+    
+    const db = client.db(dbName);
+
+      console.log("client", client);
+      if (request.method === "POST") {
+        request.on("data", (chunk) => {
+          console.log("chunk: ", request);
+          body += chunk;
+          // arr.push(body);
+          // response.end(JSON.stringify(arr));
+          // body = JSON.stringify(body);
+          // body = JSON.parse(body);
+          // db.collection("ToDoCollectionTodos").insertOne({
+          //   userId: "61c594077d2d60c58c71336a",
+          //   value: "hello world",
+          //   isChecked: false,
+          // });
+          console.log(typeof body);
+          db.collection(colletction).insertOne(JSON.parse(body));
+          response.end(JSON.stringify(body));
+        });
+      } else if (request.method === "GET") {
+        // request.on('data', (chunk) => {
+          // body = Buffer.concat(body).toString();
+          response.end(body);
+        // });
+      } else if (request.method === "DELETE") {}
+
+      if (err) {
+        console.log("can not connect to database");
+      }
+
+      // const db = client.db(dbName);
+
+      // db.collection('ToDoCollectionTodos').insertOne({
+      //     value: 'Todo',
+      //     id: 1,
+      //     checked: false
+      // })
+
+      console.log("nice");
+    }
+  );
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
-
-
--- db.js
+++ db.js
@@ -1,13 +1,88 @@
-const MongoClient = require("mongodb").MongoClient;
+// const MongoClient = require("mongodb").MongoClient;
 
-// создаем объект MongoClient и передаем ему строку подключения
-const mongoClient = new MongoClient("mongodb://127.0.0.1:27017/");
-mongoClient.connect(function (err, client) {
-  if (err) {
-    return console.log(err);
-  }
-  // взаимодействие с базой данных
-  client.close();
-});
-// "proxy": "http://127.0.0.1:3000/",
-// "Access-Control-Allow-Origin": "http://127.0.0.1:3000/",
+// // создаем объект MongoClient и передаем ему строку подключения
+// const mongoClient = new MongoClient("mongodb://127.0.0.1:27017/");
+// mongoClient.connect(function (err, client) {
+//   if (err) {
+//     return console.log(err);
+//   }
+//   // взаимодействие с базой данных
+//   client.close();
+// });
+// // "proxy": "http://127.0.0.1:3000/",
+// // "Access-Control-Allow-Origin": "http://127.0.0.1:3000/",
+
+// const MongoClient = require("mongodb").MongoClient;
+
+// const url = "mongodb://localhost:27017/";
+// const mongoClient = new MongoClient(url);
+
+// // const db = client.db("ToDoDatabase");
+
+// // Подключаемся к серверу
+// mongoClient.connect(function(err, client){
+//        debugger
+//   // обращаемся к базе данных admin
+//   const db = client.db("admin");
+//   // const db = client.db("ToDoDatabase");
+
+//   db.command({ping: 1}, function(err, result){
+//       if(!err){
+//           console.log("Подключение с сервером успешно установлено");
+//           console.log(result);
+//       }
+//       // Закрываем подключение
+//       // client.close();
+//       console.log("Подключение закрыто");
+//   });
+// });
+
+//добить подключение сервака как в ствтье https://metanit.com/web/nodejs/6.1.php
+//пересоздать базу данных(пересохранить)
+
+// const MongoClient = require("mongodb").MongoClient;
+
+// const url = "mongodb://localhost:27017/";
+// const mongoClient = new MongoClient(url);
+// async function run() {
+//   try {
+//     // Подключаемся к серверу
+//     await mongoClient.connect();
+//     // обращаемся к базе данных admin
+//     const db = mongoClient.db("admin");
+//     // выполняем пинг для проверки подключения
+//     const result = await db.command({ ping: 1 });
+//     console.log("Подключение с сервером успешно установлено");
+//     console.log(result);
+//   } catch (err) {
+//     console.log("Возникла ошибка");
+//     console.log(err);
+//   } finally {
+//     // Закрываем подключение при завершении работы или при ошибке
+//     // await mongoClient.close();
+//     console.log("Подключение закрыто");
+//   }
+// }
+// run();
+
+
+
+// export const MongoClient = require("mongodb").MongoClient;
+    
+// const url = "mongodb://localhost:27017/";
+// const mongoClient = new MongoClient(url);
+ 
+// async function run() {
+//     try {
+//         await mongoClient.connect();
+//         const db = mongoClient.db("ToDoDatabase");
+//         const collection = db.collection("ToDoCollectionUser");
+//         const count = await collection.countDocuments();
+//         console.log(`В коллекции users ${count} документов`);
+//     }catch(err) {
+//         console.log(err);
+//     } finally {
+//         await mongoClient.close();
+//     }
+// }
+// run();
