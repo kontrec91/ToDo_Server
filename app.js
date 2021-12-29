d5e98c09f8804baa9fcabb3055684d8ee2d9acfc
kontrec91 <kontrec91@gmail.com>
2021-12-29 11:10:18 +0200

    adding get and push requests for item todo

--- app.js
+++ app.js
@@ -1,67 +1,7 @@
-// import { MongoClient } from "./db.js";
-
-// const http = require("http");
-
-// const hostname = "127.0.0.1";
-// const port = 3000;
-
-// const arr = [];
-
-// const server = http.createServer((request, response) => {
-//   response.statusCode = 200;
-//   response.setHeader("Content-Type", "text/plain");
-//   response.setHeader("Content-Type", "application/json");
-//   response.setHeader("Access-Control-Allow-Origin", "*");
-//   response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-//   response.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "X-Requested-With,content-type"
-//   );
-
-//   response.setHeader("Access-Control-Allow-Credentials", true);
-
-//   let body = "";
-//   if (request.method === "POST") {
-//     request.on("data", (chunk) => {
-//       console.log('A chunk of data has arrived: ', chunk);
-//       // body += JSON.stringify(chunk);
-//       body += chunk;
-//       arr.push(body)
-//       response.end(JSON.stringify(arr));
-//     });
-//   } else if (request.method === "GET") {
-//     response.end(JSON.stringify(arr));
-//   }
-// });
-
-// server.listen(port, hostname, () => {
-//   console.log(`Server running at http://${hostname}:${port}/`);
-// });
-
-// const MongoClient = require("mongodb").MongoClient;
-// // const url = "mongodb://localhost/Blog";
-// const url = "mongodb://localhost:27017/";
-
-// console.log("yes")
-
-// http.createServer(function (req, res) {
-//   console.log(1)
-//   MongoClient.connect(url, function(err, db) {
-//       if (err) throw err;
-//       console.log(db)
-//       console.log(2)
-//   });
-// }).listen(27017);
-
-// import { createDB, getRequest } from "./db.js";
 import { getRequest } from "./db.js";
 
-import * as http from 'http';
+import * as http from "http";
 import bodyParser from "body-parser";
-
-// const http = require("http");
-// const mongoClient = require("mongodb").MongoClient;
-// const bodyParser = require("body-parser");
 
 const hostname = "127.0.0.1";
 const port = 3000;
@@ -82,24 +22,19 @@ const server = http.createServer((request, response) => {
   response.setHeader("Access-Control-Allow-Credentials", true);
 
   let body = "";
-  let collection = "ToDoCollectionTodos";
+  let collectionInDB = "ToDoCollectionTodos";
 
-  // createDB();
+  if (request.method === "POST") {
+    request.on("data", (chunk) => {
+      body += chunk;
+      getRequest(body, collectionInDB);
+      response.end(JSON.stringify(body));
+    });
+  } else if (request.method === "GET") {
+    getRequest(body, collectionInDB, response);
+  }
 
-      if (request.method === "POST") {
-        request.on("data", (chunk) => {
-          console.log("chunk: ", request);
-          body += chunk;
-          console.log(typeof body);
-          // db.collection(collection).insertOne(JSON.parse(body));
-          getRequest(body, collection);
-          response.end(JSON.stringify(body));
-        });
-      } else if (request.method === "GET") {
-      }
-
-      console.log("nice");
-
+  console.log("nice");
 });
 
 server.listen(port, hostname, () => {
--- db.js
+++ db.js
@@ -1,29 +1,22 @@
 import * as mongodb from "mongodb";
 
-// export function createDB() {
   const dbName = "ToDoDatabase";
   const uri = `mongodb://127.0.0.1:27017/${dbName}`;
 
   const mongoClient = mongodb.MongoClient;
-// }
 
-export function getRequest(body, collection) {
+export function getRequest(body, collectionInDB, response) {
   mongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
       const db = client.db(dbName);
 
-      console.log("client", client);
-      //   if (request.method === "POST") {
-      //     request.on("data", (chunk) => {
-      //       console.log("chunk: ", request);
-      //       body += chunk;
-      //       console.log(typeof body);
       if (body !== "") {
-        db.collection(collection).insertOne(JSON.parse(body));
+        db.collection(collectionInDB).insertOne(JSON.parse(body));
+      } else if(response !== undefined){
+        db.collection(collectionInDB).find().toArray((err, user)=>{
+          console.log(user);
+          response.end(JSON.stringify(user));
+        })
       }
-      //       response.end(JSON.stringify(body));
-      //     });
-      //   } else if (request.method === "GET") {
-      //   }
 
       if (err) {
         console.log("can not connect to database");
